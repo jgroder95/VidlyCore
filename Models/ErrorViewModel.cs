1e1e6ca142740668d615f38fa1ab2687cef36bdc
jacob groder <jgroder95@gmail.com>
2024-06-25 08:02:43 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VidlyCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
